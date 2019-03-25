64cec497119baf7854341e04c1a2aa9aaec25935
Florian Wyss <fw@studer-raimann.ch>
2019-03-25 10:41:59 +0100

    Fix possible config crash

--- classes/class.ilSrLpReportConfigGUI.php
+++ classes/class.ilSrLpReportConfigGUI.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . "/../vendor/autoload.php";
+
 use srag\ActiveRecordConfig\SrLpReport\ActiveRecordConfigGUI;
 use srag\Plugins\SrLpReport\Config\ConfigFormGUI;
 use srag\Plugins\SrLpReport\Utils\SrLpReportTrait;
